fecd9ebfd26d6a75270e6d2eae5037aa53f1759b
Leonel Macario <36415489+macario117@users.noreply.github.com>
2021-06-12 17:07:42 -0500

    Assignment 2.3

---
+++ Python para todos/1 Introduccion/Asignaciones/Assignment 2,3.py
@@ -0,0 +1,17 @@
+# 2.3 Escriba un programa para solicitar al usuario las horas y la tarifa por hora
+# utilizando insumos para calcular el salario bruto. Utilice 35 horas y una tasa de 2,75 por
+# hora para probar el programa (el pago debe ser 96,25). Deberías usar
+# input para leer una cadena y float () para convertir la cadena en un número. Hacer
+# no se preocupe por la comprobación de errores o datos de usuario incorrectos.
+
+hrs = input("Enter Hours:")
+hrss= float(hrs)
+cost= input("Enter the rate per hour: ")
+costo= float(cost)
+print("Pay:", costo*hrss)
+
+# Write a program to prompt the user for hours and rate per hour 
+# using input to compute gross pay. Use 35 hours and a rate of 2.75 per 
+# hour to test the program (the pay should be 96.25). You should use 
+# input to read a string and float() to convert the string to a number. Do 
+# not worry about error checking or bad user data.
