305391dfa3af22b725f2b84982fcc537807a5510
Leonel Macario <36415489+macario117@users.noreply.github.com>
2021-06-12 17:04:18 -0500

    Assignment 2.2

---
+++ Python para todos/1 Introduccion/Asignaciones/Assignment2,2.py
@@ -0,0 +1,12 @@
+# 2.2 Escriba un programa que utilice la entrada para solicitar al usuario su nombre
+# y luego les da la bienvenida. Tenga en cuenta que la entrada mostrará un cuadro de diálogo.
+# Ingrese Sarah en el cuadro emergente cuando se le solicite para que su salida
+# coincidirá con la salida deseada.
+
+name = input("Enter your name: ")
+print("Hello",name)
+
+# 2.2 Write a program that uses input to prompt a user for their name 
+# and then welcomes them. Note that input will pop up a dialog box. 
+# Enter Sarah in the pop-up box when you are prompted so your output 
+# will match the desired output.
